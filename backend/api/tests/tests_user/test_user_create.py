293ec5c4b6d55631933d26457a9ad1ba720e030a
Lucas Camargo <lfqcamargo@gmail.com>
2024-06-13 21:19:35 -0300

    added password verification test

--- backend/api/tests/tests_user/test_user_create.py
+++ backend/api/tests/tests_user/test_user_create.py
@@ -8,9 +8,13 @@ from datetime import datetime, timedelta, timezone
 import pytest
 from httpx import AsyncClient
 from fastapi import status
+from sqlalchemy.future import select
+
+from api.modules.users.models.User import User
+from api.utils.crypt_password import verify_password
 
 @pytest.mark.asyncio
-async def test_create_new_user(client: AsyncClient) -> None:
+async def test_create_new_user(client: AsyncClient, setup_database) -> None:
     """
     Test the creation of a user.
 
@@ -19,7 +23,7 @@ async def test_create_new_user(client: AsyncClient) -> None:
     user_data = {
         "email": "user@example.com",
         "cpf_cnpj": "88877936037", 
-        "whatsapp": "14991000000",
+        "whatsapp": "14991396707",
         "name": "UserExample",
         "password": "securepassword",
         "sex": "M",
@@ -36,6 +40,12 @@ async def test_create_new_user(client: AsyncClient) -> None:
     assert body["name"] == user_data["name"]
     assert body["sex"] == user_data["sex"]
     assert body["date_birthday"] == user_data["date_birthday"]
+
+    async with setup_database.begin():
+        user = await setup_database.execute(select(User).where(User.email == user_data["email"]))
+        user = user.scalar_one()
+
+    assert verify_password("securepassword", user.password)
 
     date_created_api = datetime.fromisoformat(
         body['date_created'].rstrip('Z')).replace(tzinfo=timezone.utc)
@@ -98,6 +108,33 @@ async def test_invalid_cpf_cnpj(client: AsyncClient, cpf_cnpj: str) -> None:
         "email": "user@example.com",
         "cpf_cnpj": cpf_cnpj, 
         "whatsapp": "14991000000",
+        "name": "UserExample",
+        "password": "securepassword",
+        "sex": "M",
+        "date_birthday": "1990-01-01" 
+    }
+
+    response = await client.post("/users/", json=user_data)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("whatsapp", [
+    "149900000",       # Número muito curto
+    "phone_number",    # Contém letras
+    "+55149900000000", # Número muito longo
+    "149900000000",    # Excede o tamanho máximo permitido
+    "00000000000",     # Número não realista
+    "1499-000-000",    # Formato incorreto com hífens
+    "+1 (499) 000-000" # Formato internacional errado
+])
+async def test_invalid_whatsapp(client: AsyncClient, whatsapp: str) -> None:
+    """
+    Test various invalid WhatsApp number formats.
+    """
+    user_data = {
+        "email": "user@example.com",
+        "cpf_cnpj": "88877936037", 
+        "whatsapp": whatsapp,
         "name": "UserExample",
         "password": "securepassword",
         "sex": "M",
--- backend/api/utils/crypt_password.py
+++ backend/api/utils/crypt_password.py
@@ -19,3 +19,18 @@ def has_password(password: str) -> str:
     password_bytes = password.encode('utf-8')
     hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
     return hashed_password.decode('utf-8')
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    """
+    Verifies a hashed password against a password provided by the user.
+
+    Args:
+        password (str): The plaintext password to verify.
+        hashed_password (str): The hashed password to compare against.
+
+    Returns:
+        bool: True if the password matches the hashed password, False otherwise.
+    """
+    password_bytes = password.encode('utf-8')
+    hashed_password_bytes = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(password_bytes, hashed_password_bytes)
